fbbcd7e41927b05a65e1dcdf41e04d19cbbde53d
Laura Krupa <laura.krupa@astellas.com>
2022-09-01 15:58:10 +0200

    stopper

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import './App.css';
+import Button from './components/Button/Button';
+import Time from './components/Time/Time';
+import { useState, useEffect } from "react"
+
+const App = () => {
+
+  const [time, setTime] = useState(0)
+  const [timer, setTimer]= useState(null)
+
+  
+
+  const start = ()=>{
+    setTimer(setInterval(() => {
+      setTime(prevValue => prevValue + 1);
+    }, 1))
+     
+    
+  };
+  
+  const stop=()=>{
+    
+      clearInterval(timer);
+      setTimer(null);
+      console.log('stop');
+  }
+  const reset = () =>{
+    setTime(0) 
+    stop()
+  }
+
+  useEffect(() => {
+
+    return ()=>{
+
+    }
+  },[time])
+  
+
+  return (
+        <div className="App">
+          <Time time={time}/>
+          <Button start={start} stop={stop} reset={reset}/>
+        </div>
+    );
+}
+export default App;
---
+++ src/components/Time/Time.js
@@ -0,0 +1,44 @@
+import Style from './Time.module.scss'
+
+const Time = ({time}) =>{
+
+    const plusO =(abc) =>{
+        if(abc < 10){
+            return(
+                '0'+ abc
+            )
+        }
+        return(
+            abc
+        )}
+
+    const hour = () =>{
+        const hr = Math.floor(time/60000 )
+        return(
+            hr
+        )}
+   
+    const minut = () =>{
+        const min = Math.floor(time/6000 )
+        return(
+            min
+        )}
+   
+    const second = () =>{
+        let sec = Math.floor(time/100) 
+        if(sec === 60){
+            return(
+                sec= 0
+            )} 
+        return(
+            sec
+        )}  
+         
+    return(
+        <div className={Style.abcd}>
+            {plusO(hour())}:{plusO(minut() % 60)}:{plusO(second() % 60)}.{time % 100} 
+        </div>
+    );
+}
+
+export default Time;
